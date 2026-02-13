4c5739276aff386192e5c70e92fa085558da6f74
Abhishek Mishra <167686628+AeroByte-Abhishek@users.noreply.github.com>
2026-02-13 14:59:31 +0530

    Disable RapidResumeApplicationTests class
    Comment out the RapidResumeApplicationTests class and its methods.

--- src/test/java/com/example/RapidResume/RapidResumeApplicationTests.java
+++ src/test/java/com/example/RapidResume/RapidResumeApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.RapidResume;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class RapidResumeApplicationTests {
+// @SpringBootTest
+// class RapidResumeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
